0eff01782c2f44fd6bdd011ccf16ead8f03f0b18
vthirtythr33 <aaronhui88@gmail.com>
2025-05-23 05:54:22 -0400

    help and adding guilds

--- main.py
+++ main.py
@@ -37,10 +37,12 @@ token = os.getenv('DISCORD_TOKEN')
 class Client(commands.Bot):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+        guild_ids = [761274425475072010,1163677315553824768]
         try:
-            guild = discord.Object(id=761274425475072010)
-            synced = await self.tree.sync(guild=guild)
-            print(f'Synced {len(synced)} commands to guild {guild.id}')
+            for guild_id in guild_ids:
+                guild = discord.Object(id=guild_id)
+                synced = await self.tree.sync(guild=guild)
+                print(f'Synced {len(synced)} commands to guild {guild.id}')
         except Exception as e:
             print(f'Error syncing commands: {e}')
 
@@ -48,125 +50,129 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 intents.presences = True
-client = Client(command_prefix="!", intents=intents)
+client = Client(command_prefix="!", intents=intents, activity = discord.Game(name="TETR.IO"), status=discord.Status.idle)
 
-GUILD_ID = discord.Object(id=761274425475072010)
+GUILD_IDS = [discord.Object(id=761274425475072010), discord.Object(id=1163677315553824768)]
+    
+for GUILD_ID in GUILD_IDS:
+    @client.tree.command(name="help", description="Display a list of all commands.", guild=GUILD_ID)
+    async def help(interaction: discord.Interaction):
+        embed = discord.Embed(title="Help", color=discord.Color.purple())
+        embed.add_field(name="/help", value="Display this message!", inline=False)
+        embed.add_field(name="/roster `[team]`", value="Display the roster of any team.", inline=False)
+        embed.add_field(name="/standings `[league]` `[team]` `[shown]`", value="Display the standings or the record of a specific team. Entry for `team` overrides all other values. `league` defaults to upper league, `shown` defaults to 10.")
+        await interaction.response.send_message(embed=embed)
 
-@client.tree.command(name="help", description="Display a list of all commands.", guild=GUILD_ID)
-async def help(interaction: discord.Interaction):
-     await interaction.response.send_message("Displaying a list of all commands.")
-     # later send list of commands
-
-# getting a 403 for some reason so i'll figure it out later
-'''
-@client.tree.command(name="getstats", description="Get the stats of any TETR.IO user!", guild=GUILD_ID)
-async def getStats(interaction: discord.Interaction, username: str):
-     url = f"https://ch.tetr.io/api/users/{username.lower()}/summaries/league"
-     response = requests.get(url)
-     if response.status_code == 200:
-         rank = "rank"
-         tr = "tr"
-         glicko = "glicko"
-         apm = "apm"
-         pps = "pps"
-         vs = "vs"
-         await interaction.response.send_message(f'{username.lower()} stats:\nRank: {rank}\nTR: {tr}\nGlicko: {glicko}\nAPM: {apm}\nPPS:{pps}\nVS:{vs}')
-     else:
-         await interaction.response.send_message(response.status_code)
-'''
-
-@client.tree.command(name="standings", description="Display standings for either the upper league or lower league.", guild=GUILD_ID)
-@app_commands.describe(
-    league="(Optional) Choose a league to display standings for. Defaults to upper if no team is indicated.",
-    team="(Optional) Choose a team to display standings.",
-    shown="(Optional) Choose the number of teams shown. Defaults to 10 teams. 0 means show all."
-)
-@app_commands.choices(
-    league=[
-        app_commands.Choice(name="upper", value="upper"),
-        app_commands.Choice(name="lower", value="lower")
-    ]
-)
-async def standings(interaction: discord.Interaction, league: app_commands.Choice[str] = None, team: str = None, shown: str = "10"):
-    maxTeams = 0 # placeholder for max teams there are
-    if team is None:
-        if (league is None or league.name == "upper"):
-            embed = discord.Embed(title="2026 Upper League Standings", color=discord.Color.purple())
-            embed.set_thumbnail(url="https://cdn.discordapp.com/icons/829182450185142312/f78172b9c494c081f4c4ca6da29b76e2.png?size=1024")
-            embed.add_field(
-                name="Team",
-                value="\n".join([f"**{index+1}: {university}** ({score} Points, {bucholz} Bucholz, {point_differential} Point Differential)" 
-                             for index, (university, score, bucholz, point_differential,games_played) in enumerate(upperStandings[:min(int(shown),10)])]),
-                inline=True
-            ) # fix with buttons later
-            await interaction.response.send_message(embed=embed)
+    # getting a 403 for some reason so i'll figure it out later
+    '''
+    @client.tree.command(name="getstats", description="Get the stats of any TETR.IO user!", guild=GUILD_ID)
+    async def getStats(interaction: discord.Interaction, username: str):
+        url = f"https://ch.tetr.io/api/users/{username.lower()}/summaries/league"
+        response = requests.get(url)
+        if response.status_code == 200:
+            rank = "rank"
+            tr = "tr"
+            glicko = "glicko"
+            apm = "apm"
+            pps = "pps"
+            vs = "vs"
+            await interaction.response.send_message(f'{username.lower()} stats:\nRank: {rank}\nTR: {tr}\nGlicko: {glicko}\nAPM: {apm}\nPPS:{pps}\nVS:{vs}')
         else:
-            embed = discord.Embed(title="2026 Lower League Standings", color=discord.Color.purple())
+            await interaction.response.send_message(response.status_code)
+    '''
+
+    @client.tree.command(name="standings", description="Display standings for either the upper league or lower league.", guild=GUILD_ID)
+    @app_commands.describe(
+        league="(Optional) Choose a league to display standings for. Defaults to upper if no team is indicated.",
+        team="(Optional) Choose a team to display standings.",
+        shown="(Optional) Choose the number of teams shown. Defaults to 10 teams. 0 means show all."
+    )
+    @app_commands.choices(
+        league=[
+            app_commands.Choice(name="upper", value="upper"),
+            app_commands.Choice(name="lower", value="lower")
+        ]
+    )
+    async def standings(interaction: discord.Interaction, league: app_commands.Choice[str] = None, team: str = None, shown: str = "10"):
+        maxTeams = 0 # placeholder for max teams there are
+        if team is None:
+            if (league is None or league.name == "upper"):
+                embed = discord.Embed(title="2026 Upper League Standings", color=discord.Color.purple())
+                embed.set_thumbnail(url="https://cdn.discordapp.com/icons/829182450185142312/f78172b9c494c081f4c4ca6da29b76e2.png?size=1024")
+                embed.add_field(
+                    name="Team",
+                    value="\n".join([f"**{index+1}: {university}** ({score} Points, {bucholz} Bucholz, {point_differential} Point Differential)" 
+                                for index, (university, score, bucholz, point_differential,games_played) in enumerate(upperStandings[:min(int(shown),10)])]),
+                    inline=True
+                ) # fix with buttons later
+                await interaction.response.send_message(embed=embed)
+            else:
+                embed = discord.Embed(title="2026 Lower League Standings", color=discord.Color.purple())
+                embed.set_thumbnail(url="https://cdn.discordapp.com/icons/829182450185142312/f78172b9c494c081f4c4ca6da29b76e2.png?size=1024")
+                embed.add_field(
+                    name="Team",
+                    value="\n".join([f"**{index+1}: {university}** ({score} Points, {bucholz} Bucholz, {point_differential} Point Differential)" 
+                        for index, (university, score, bucholz, point_differential,games_played) in enumerate(lowerStandings[:min(int(shown),10)])]),
+                    inline=True
+                ) # fix with buttons later
+                await interaction.response.send_message(embed=embed)
+        else:
+            def find_university_info(university_name):
+                for index, (university, score, bucholz, point_differential, games_played) in enumerate(lowerStandings + upperStandings):
+                    if university_name == university:
+                        if (index+1-len(lowerStandings))>0:
+                            return index+1-len(lowerStandings), score, bucholz, point_differential, games_played
+                        else:
+                            return index+1, score, bucholz, point_differential, games_played
+            rank, score, bucholz, point_differential, games_played = find_university_info(team)
+            embed = discord.Embed(title=f'2026 {team} Standings', color=discord.Color.purple())
             embed.set_thumbnail(url="https://cdn.discordapp.com/icons/829182450185142312/f78172b9c494c081f4c4ca6da29b76e2.png?size=1024")
-            embed.add_field(
-                name="Team",
-                value="\n".join([f"**{index+1}: {university}** ({score} Points, {bucholz} Bucholz, {point_differential} Point Differential)" 
-                     for index, (university, score, bucholz, point_differential,games_played) in enumerate(lowerStandings[:min(int(shown),10)])]),
-                inline=True
-            ) # fix with buttons later
+            embed.add_field(name="Record", value=f'{score}-{int(games_played)-int(score)}', inline=True)
+            embed.add_field(name="Rank", value=rank, inline=True)
+            embed.add_field(name="", value="", inline=False)
+            embed.add_field(name="Bucholz", value=bucholz, inline=True)
+            embed.add_field(name="Point Differential", value=point_differential, inline=True)
             await interaction.response.send_message(embed=embed)
-    else:
-         def find_university_info(university_name):
-            for index, (university, score, bucholz, point_differential, games_played) in enumerate(lowerStandings + upperStandings):
-                if university_name == university:
-                    if (index+1-len(lowerStandings))>0:
-                        return index+1-len(lowerStandings), score, bucholz, point_differential, games_played
-                    else:
-                        return index+1, score, bucholz, point_differential, games_played
-         rank, score, bucholz, point_differential, games_played = find_university_info(team)
-         embed = discord.Embed(title=f'2026 {team} Standings', color=discord.Color.purple())
-         embed.set_thumbnail(url="https://cdn.discordapp.com/icons/829182450185142312/f78172b9c494c081f4c4ca6da29b76e2.png?size=1024")
-         embed.add_field(name="Record", value=f'{score}-{int(games_played)-int(score)}', inline=True)
-         embed.add_field(name="Rank", value=rank, inline=True)
-         embed.add_field(name="", value="", inline=False)
-         embed.add_field(name="Bucholz", value=bucholz, inline=True)
-         embed.add_field(name="Point Differential", value=point_differential, inline=True)
-         await interaction.response.send_message(embed=embed)
 
-@standings.autocomplete('team')
-async def standings_autocomplete(interaction: discord.Interaction, current: str):
-    filtered = [team for team in teams if current.lower() in team.lower()]
-    limited = filtered[:25]
-    return [
-        app_commands.Choice(name=team, value=team)
-        for team in limited
-    ]
+    @standings.autocomplete('team')
+    async def standings_autocomplete(interaction: discord.Interaction, current: str):
+        filtered = [team for team in teams if current.lower() in team.lower()]
+        limited = filtered[:25]
+        return [
+            app_commands.Choice(name=team, value=team)
+            for team in limited
+        ]
 
-@client.tree.command(name="roster", description="Display the full roster of a team.", guild=GUILD_ID)
-@app_commands.describe(team="Select a team.")
-async def roster(interaction: discord.Interaction, team: str):
-    def find_university_roster(university_name):
-            for info in rosters:
-                if info[0] == university_name:
-                    return info
-    embed = discord.Embed(title=f'2026 {team} Roster', color=discord.Color.purple())
-    embed.set_thumbnail(url="https://cdn.discordapp.com/icons/829182450185142312/f78172b9c494c081f4c4ca6da29b76e2.png?size=1024")
-    embed.add_field(
-        name="Starters",
-        value="\n".join(f'**{player}**'
-            for player in find_university_roster(team)[3:8]),
-        inline=True
-    )
-    embed.add_field(
-        name="Substitutes",
-        value="\n".join(f'**{player}**'
-            for player in find_university_roster(team)[13:18]),
-        inline=True
-    )
-    await interaction.response.send_message(embed=embed)
+    @client.tree.command(name="roster", description="Display the full roster of a team.", guild=GUILD_ID)
+    @app_commands.describe(team="Select a team.")
+    async def roster(interaction: discord.Interaction, team: str):
+        def find_university_roster(university_name):
+                for info in rosters:
+                    if info[0] == university_name:
+                        return info
+        embed = discord.Embed(title=f'2026 {team} Roster', color=discord.Color.purple())
+        embed.set_thumbnail(url="https://cdn.discordapp.com/icons/829182450185142312/f78172b9c494c081f4c4ca6da29b76e2.png?size=1024")
+        embed.add_field(
+            name="Starters",
+            value="\n".join(f'**[{player}](https://ch.tetr.io/u/{player})**'
+                for player in find_university_roster(team)[3:8]),
+            inline=True
+        )
+        embed.add_field(
+            name="Substitutes",
+            value="\n".join(f'**[{player}](https://ch.tetr.io/u/{player})**'
+                for player in find_university_roster(team)[13:18]),
+            inline=True
+        )
+        await interaction.response.send_message(embed=embed)
 
-@roster.autocomplete('team')
-async def roster_autocomplete(interaction: discord.Interaction, current: str):
-    filtered = [team for team in teams if current.lower() in team.lower()]
-    limited = filtered[:25]
-    return [
-        app_commands.Choice(name=team, value=team)
-        for team in limited
-    ]
+    @roster.autocomplete('team')
+    async def roster_autocomplete(interaction: discord.Interaction, current: str):
+        filtered = [team for team in teams if current.lower() in team.lower()]
+        limited = filtered[:25]
+        return [
+            app_commands.Choice(name=team, value=team)
+            for team in limited
+        ]
 
 client.run(token)
